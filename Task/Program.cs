e59e0546c916d170a54b22250fac6430f730220d
Ivan Maestro <masterov_in@mail.ru>
2022-09-18 15:28:18 +0300

    create finding method

--- Task/Program.cs
+++ Task/Program.cs
@@ -5,5 +5,25 @@ string text = Console.ReadLine()
 
 string[] data = text.Split(' ');
 
+string[] CheckElemLenght(string[] arr)
+{
+    int count = 0;
+    int newTextLenght = 0;
+    for (int j = 0; j < arr.Length; j++)
+    {
+        if (arr[j].Length <= 3) newTextLenght++;
+    }
+    string[] newText = new string[newTextLenght];
 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newText[count] = arr[i];
+            count++;
+        }
+        else continue;
+    }
+    return newText;
+}
 
